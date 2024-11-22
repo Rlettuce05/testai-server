2e571d42b1789a6f39ac87d596debbb1c190efda
RLettuce <syunrobin@gmail.com>
2024-11-22 10:56:42 +0900

    🔧api changed for object detection

--- src.py
+++ src.py
@@ -1,32 +1,66 @@
-from tensorflow.keras.models import load_model
+from torch import load, tensor
+import torch
 from PIL import Image
+import PIL.ImageDraw as ImageDraw
 import os
 import numpy as np
 import base64
+from torchvision.models.detection import fasterrcnn_resnet50_fpn
+from torchvision.models.detection.faster_rcnn import FastRCNNPredictor
+from torchvision import transforms
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-MODEL_DIR = os.path.join(BASE_DIR, "JupyterNotebook/saved_model")
-MODEL_FILE = os.path.join(MODEL_DIR, "model.h5")
+MODEL_FILE = os.path.join("./net.prm")
 
 
 class Service:
-    model = load_model(MODEL_FILE)
+    # 学習済みモデルの読み込み
+    model = fasterrcnn_resnet50_fpn(weights = "COCO_V1", weights_backbone='DEFAULT')
+    model.roi_heads.box_predictor = FastRCNNPredictor(model.roi_heads.box_predictor.cls_score.in_features, 3)
+    model_weight = torch.load(MODEL_FILE)
+    model.load_state_dict(model_weight)
+    model.cpu()
+    model.eval()
 
     @staticmethod
     def predict(data):
-        label_predict = {0:"飛行機", 1:"自動車", 2:"鳥", 3:"猫", 4:"鹿", 5:"犬", 6:"カエル", 7:"馬", 8:"船", 9:"トラック"}
+        # fasterRCNNを使って信号機の予測を行い，画像を返す
+        label_predict = ["background", "GREEN", "RED"]
         img_binary = base64.b64decode(data["img"][22:])
         with open("temp.png", 'bw') as f:
             f.write(img_binary)
-        img = np.array(Image.open("temp.png").convert("RGB"))
-        img = img.reshape(1, 32, 32, 3)
+        image = Image.open("temp.png").convert("RGB")
+        trans = [
+            transforms.ToTensor(),
+            transforms.Normalize(0.5, 0.5)
+        ]
+        trans = transforms.Compose(trans)
+        img = trans(image)
+        img = img.to("cpu")
         print(img)
         print(img.shape)
-        predict_num = Service.model.predict(img)
-        predict_num = np.argmax(predict_num[0])
-        label = label_predict[predict_num]
-        print(label)
-        return label
+        outputs = Service.model([img])
+        outputs = [{k: v.to('cpu').detach().numpy() for k, v in t.items()} for t in outputs]
+        outputs = outputs[0]
+        print(outputs)
+
+        draw = ImageDraw.Draw(image)
+        labels = ""
+        for i in range(len(outputs["labels"])):
+            if outputs["scores"][i] > 0.5:
+                l = outputs["labels"][i]
+                color = "red" if l == 2 else "blue"
+                box = outputs["boxes"][i]
+                l = label_predict[int(l)]
+                labels += labels + l + " "
+                draw.rectangle([(box[0], box[1]), (box[2], box[3])], outline=color, width=3)
+        image.save("output.png")
+
+        with open("output.png", "rb") as f:
+            img_binary = f.read()
+        result = base64.b64encode(img_binary).decode("utf-8")
+
+        return {"result": result, "label": labels}
 
 class Resource:
     @staticmethod
